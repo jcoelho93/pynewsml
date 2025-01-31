708a9b38fb36c198f1e8061f789fc937a5f37f20
José Coelho <16445494+jcoelho93@users.noreply.github.com>
2025-01-31 09:13:27 +0000

    Add docs

--- tests/test_newsml.py
+++ tests/test_newsml.py
@@ -10,16 +10,6 @@ class TestNewsML(TestCase):
         newsml = NewsML.from_xml(content)
 
         self.assertEqual(len(newsml.news_items), 1)
-        news_item = newsml.news_items[0]
-        self.assertEqual(news_item.identifier.provider_id, "AFP")
-        self.assertEqual(news_item.identifier.date_id, "20210701")
-        self.assertEqual(news_item.identifier.news_item_id, "50812345")
-        self.assertEqual(news_item.identifier.revision_id.value, "1")
-        self.assertEqual(news_item.identifier.revision_id.update, "N")
-        self.assertEqual(
-            news_item.identifier.revision_id.previous_revision, "0")
-        self.assertEqual(news_item.identifier.public_identifier,
-                         "urn:newsml:afp.com:20210701:50812345")
 
 
 if __name__ == '__main__':
